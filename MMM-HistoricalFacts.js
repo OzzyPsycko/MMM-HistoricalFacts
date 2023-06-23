d327d2645376b9d51ac775aad91d0a67345bc146
OzzyPsycko <40775335+OzzyPsycko@users.noreply.github.com>
2023-06-23 12:48:23 +0100

    Create MMM-HistoricalFacts.js

---
+++ MMM-HistoricalFacts.js
@@ -0,0 +1,28 @@
+Module.register("MMM-HistoricalFacts", {
+  // Module configuration options
+  defaults: {
+    updateInterval: 2 * 60 * 1000, // 2 minutes
+  },
+
+  // Define start sequence.
+  start: function() {
+    Log.info("Starting module: " + this.name);
+    this.sendSocketNotification("START_FETCHING_FACTS");
+  },
+
+  // Override the dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.className = "small";
+    wrapper.innerHTML = this.fact || this.translate("LOADING");
+    return wrapper;
+  },
+
+  // Handle incoming socket notifications.
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "FACT_FETCHED") {
+      this.fact = payload;
+      this.updateDom();
+    }
+  },
+});
